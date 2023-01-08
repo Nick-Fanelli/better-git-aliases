10f0fdcd250186b44a9fd8df27eefddb23ed24f4
Nick Fanelli <nickfanelli5@gmail.com>
2023-01-08 14:19:01 -0500

    feat: add better log

--- macos.sh
+++ macos.sh
@@ -1,1 +1,3 @@
 git config --global alias.cm '!git add .; git commit -m'
+git config --global alias.logg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
