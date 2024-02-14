7168834895eff62005939dd3c762c7d0c195b851
Nick Fanelli <41591082+Nick-Fanelli@users.noreply.github.com>
2024-02-13 19:17:24 -0500

    feat: delete branch

--- better-git-aliases.sh
+++ better-git-aliases.sh
@@ -12,3 +12,5 @@ git config --global alias.logg "log --color --graph --pretty=format:'%Cred%h%Cre
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
 git config --global alias.stat "status"
+
+git config --global alias.del-branch 'push origin --delete'
